fa4cf9b117f9ad4c7a41951de8a842dc0053921a
EC2 Default User <ec2-user@ip-172-31-32-215.ap-northeast-1.compute.internal>
2022-07-12 10:29:08 +0000

    [add] 3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,18 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     @book = Book.new(book_params)
     if @book.save
       redirect_to book_path(@book.id), notice: 'Book was successfully created.'
     else
-      render :show
+      @books = Book.all
+      render :index
     end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -35,15 +34,12 @@ class BooksController < ApplicationController
 
   def destroy
     book = Book.find(params[:id])
-    if book.destroy
-      redirect_to '/books', notice: 'Book was successfully destroyed.'
-    else
-      render :index
-    end
+    book.destroy
+    redirect_to '/books', notice: 'Book was successfully destroyed.'
   end
 
   private
   def book_params
-    params.require(:book).permit(:title, :body)
+    params[:book].permit(:title, :body)
   end
 end
